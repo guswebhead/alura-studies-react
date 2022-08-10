e4d4146e6f9ec3f185438bfddac194a6adc27e4b
Gustavo Caixeta <gustavo.caixeta42@gmail.com>
2022-08-10 00:22:25 -0300

    :sparkles: feat: novo componente relógio dentro de cronometro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from '../logo.svg';
-import Botao from '../components/Botao';
 import Lista from '../components/Lista';
 import Formulario from '../components/Formulario';
 import style from './App.module.scss'
+import { Cronometro } from '../components/Cronometro';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario/>
       <Lista/>
+      <Cronometro/>
     </div>
   );
 }
